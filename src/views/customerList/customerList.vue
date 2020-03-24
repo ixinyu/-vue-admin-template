68c53851ef0ce5091bdfd2defa1e2092abf63a1c
jbb <2736027872@qq.com>
2020-03-24 14:52:11 +0800

    基于vue-element-templete 开发后台系统，权限控制，tagviews

---
+++ src/views/customerList/customerList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="c-wrap">
+    <div class="content" />
+    <el-popover
+      v-model="visible"
+      placement="top"
+      title="标题"
+      width="200"
+      trigger="click"
+      content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。"
+    >
+      <el-button slot="reference">click 激活</el-button>
+    </el-popover>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人" />
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai" />
+          <el-option label="区域二" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+      <el-button type="primary" style="float:right;" @click="onSubmit">添加</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Customer',
+  data() {
+    return {
+      visible: false,
+      formInline: {
+        user: '',
+        region: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.content {
+  width: 80%;
+  height: 100%;
+  margin: 60px auto 0;
+  .pdf {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text" @click="goCustomer">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  mounted() {
+  },
+  methods: {
+    goCustomer() {
+      this.$router.push({ path: '/customer/index' })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
